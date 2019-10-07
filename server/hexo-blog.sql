a01e18ca8000352f4ef40ea085bf468e72c669cf
移影残风 <yiyingcanfeng@qq.com>
2019-10-07 15:46:34 +0800

    sql脚本增加测试数据

--- server/hexo-blog.sql
+++ server/hexo-blog.sql
@@ -17,6 +17,11 @@ CREATE TABLE `admin`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of admin
+-- ----------------------------
+INSERT INTO `admin` VALUES (1, 'admin', '管理员', '6fc3f369cc8bc7689c24c637109c0801d15bbb176e1a86f43e3c0947fa9a5114', '');
+
+-- ----------------------------
 -- Table structure for article
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
@@ -26,6 +31,34 @@ CREATE TABLE `article`  (
   `create_time` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 68 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of article
+-- ----------------------------
+INSERT INTO `article` VALUES (425, '测试文章01', '2019-07-01 00:00:00');
+INSERT INTO `article` VALUES (428, '测试文章02', '2019-07-02 00:00:00');
+INSERT INTO `article` VALUES (430, '测试文章03', '2019-07-03 00:00:00');
+INSERT INTO `article` VALUES (433, '测试文章04', '2019-07-04 00:00:00');
+INSERT INTO `article` VALUES (436, '测试文章05', '2019-07-05 00:00:00');
+INSERT INTO `article` VALUES (439, '测试文章06', '2019-07-06 00:00:00');
+INSERT INTO `article` VALUES (443, '测试文章07', '2019-07-07 00:00:00');
+INSERT INTO `article` VALUES (446, '测试文章08', '2019-07-08 00:00:00');
+INSERT INTO `article` VALUES (448, '测试文章09', '2019-07-09 00:00:00');
+INSERT INTO `article` VALUES (452, '测试文章10', '2019-07-10 00:00:00');
+INSERT INTO `article` VALUES (455, '测试文章11', '2019-07-11 00:00:00');
+INSERT INTO `article` VALUES (457, '测试文章12', '2019-07-12 00:00:00');
+INSERT INTO `article` VALUES (461, '测试文章13', '2019-07-13 00:00:00');
+INSERT INTO `article` VALUES (464, '测试文章14', '2019-07-14 00:00:00');
+INSERT INTO `article` VALUES (466, '测试文章15', '2019-07-15 00:00:00');
+INSERT INTO `article` VALUES (469, '测试文章16', '2019-07-16 00:00:00');
+INSERT INTO `article` VALUES (474, '测试文章17', '2019-07-17 00:00:00');
+INSERT INTO `article` VALUES (476, '测试文章18', '2019-07-18 00:00:00');
+INSERT INTO `article` VALUES (479, '测试文章19', '2019-07-19 00:00:00');
+INSERT INTO `article` VALUES (481, '测试文章20', '2019-07-20 00:00:00');
+INSERT INTO `article` VALUES (485, '测试文章21', '2019-07-21 00:00:00');
+INSERT INTO `article` VALUES (491, '测试文章23', '2019-07-23 00:00:00');
+INSERT INTO `article` VALUES (493, '测试文章22', NULL);
+INSERT INTO `article` VALUES (494, '测试', NULL);
 
 -- ----------------------------
 -- Table structure for comment
@@ -44,6 +77,14 @@ CREATE TABLE `comment`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 37 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES (1, '可以', '2019-10-07 15:35:15', 1, 491, 0, 0, 1);
+INSERT INTO `comment` VALUES (2, '写的不错', '2019-10-07 15:35:49', 2, 491, 0, 0, 1);
+INSERT INTO `comment` VALUES (3, '确实', '2019-10-07 15:36:53', 3, 491, 1, 1, 1);
+INSERT INTO `comment` VALUES (4, '不错', '2019-10-07 15:37:32', 10, 491, 0, 0, 0);
+
+-- ----------------------------
 -- Table structure for sys_config
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_config`;
@@ -56,6 +97,11 @@ CREATE TABLE `sys_config`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of sys_config
+-- ----------------------------
+INSERT INTO `sys_config` VALUES (1, '/root/hexo-blog/source/_posts', '/root/hexo-blog', '/usr/share/nginx/html');
+
+-- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
@@ -66,5 +112,18 @@ CREATE TABLE `user`  (
   `website` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, '张三', 'aaa@qq.com', 'aaa.com');
+INSERT INTO `user` VALUES (2, '李五', 'bbb@qq.com', NULL);
+INSERT INTO `user` VALUES (3, '王五', 'ccc@qq.com', 'ccc.com');
+INSERT INTO `user` VALUES (10, '赵六', 'ddd@qq.com', NULL);
+INSERT INTO `user` VALUES (11, '小张', 'z@qq.com', NULL);
+INSERT INTO `user` VALUES (12, '小李', 'xl@qq.com', NULL);
+INSERT INTO `user` VALUES (14, '小王', 'wang@qq.com', NULL);
+INSERT INTO `user` VALUES (15, '小赵', 'zhao@qq.com', NULL);
+INSERT INTO `user` VALUES (16, '李', 'li@qq.com', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
